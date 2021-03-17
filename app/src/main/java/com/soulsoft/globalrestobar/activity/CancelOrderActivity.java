b00b1939bf461e81e2aa25a3ceec2fb84e3a9b38
Rockstar <pardeshisantosh254@gmail.com>
2021-03-18 00:36:21 +0530

    Worked on CancelOrderActivity.java

---
+++ app/src/main/java/com/soulsoft/globalrestobar/activity/CancelOrderActivity.java
@@ -0,0 +1,144 @@
+package com.soulsoft.globalrestobar.activity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.soulsoft.globalrestobar.BaseActivity;
+import com.soulsoft.globalrestobar.R;
+import com.soulsoft.globalrestobar.adapter.CancelOrderDetailsAdapter;
+import com.soulsoft.globalrestobar.adapter.RunningOrderDetailsAdapter;
+import com.soulsoft.globalrestobar.model.existingkot.ExistingDetailsResponse;
+import com.soulsoft.globalrestobar.model.existingkot.ExistingKotBO;
+import com.soulsoft.globalrestobar.utility.AllKeys;
+import com.soulsoft.globalrestobar.utility.CommonMethods;
+import com.soulsoft.globalrestobar.utility.ConfigUrl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+
+public class CancelOrderActivity extends BaseActivity {
+
+    private static final String TAG = "CancelOrderActivity";
+    @BindView(R.id.et_search)
+    EditText etSearch;
+    @BindView(R.id.rv_running_order_details)
+    RecyclerView rvRunningOrderDetails;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
+    private ArrayList<ExistingKotBO> existingKotBOArrayList=new ArrayList<>();
+    CancelOrderDetailsAdapter cancelOrderDetailsAdapter;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //basic intialisation...
+        initViews();
+
+        if(getIntent().getStringExtra("tableno")!=null){
+            //get order details of corresponding table no...
+            if(CommonMethods.isNetworkAvailable(mContext)){
+                getOrderDetailsOfTable(getIntent().getStringExtra("tableno"));
+            }
+        }
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filterTable(s.toString());
+            }
+        });
+    }
+
+    @Override
+    protected int getActivityLayout( ) {
+        return R.layout.activity_running_order_details;
+    }
+
+    private void initViews(){
+        //Setting text of toolbar..
+        tvTitle.setText(getResources().getString(R.string.cancel_kot));
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(mContext);
+        rvRunningOrderDetails.setLayoutManager(linearLayoutManager);
+    }
+
+    private void getOrderDetailsOfTable(String tableNo){
+        final ProgressDialog progressDialog = new ProgressDialog(mContext);
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setTitle("Fetching data");
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, CommonMethods.getPrefrence(mContext, AllKeys.BASE_URL).concat(ConfigUrl.BOOKEDTABLE_KOTDETAILS), response -> {
+            Log.e(TAG, "onResponse: " + response);
+            Gson gson = new Gson();
+
+            ExistingDetailsResponse existingDetailsResponse = gson.fromJson(response, ExistingDetailsResponse.class);
+            progressDialog.dismiss();
+            if(existingDetailsResponse.getTable().size()>0){
+                existingKotBOArrayList.addAll(existingDetailsResponse.getTable());
+                // llExistingOrder.setVisibility(View.VISIBLE);
+                cancelOrderDetailsAdapter=new CancelOrderDetailsAdapter(mContext,existingKotBOArrayList);
+                rvRunningOrderDetails.setAdapter(cancelOrderDetailsAdapter);
+            }else{
+                // llExistingOrder.setVisibility(View.GONE);
+            }
+        }, error -> {
+            progressDialog.dismiss();
+            Log.e(TAG, "onErrorResponse: " + error);
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("TableNo",tableNo);
+                Log.e(TAG, "getParams: " + params);
+                return params;
+            }
+        };
+        RequestQueue mQueue = Volley.newRequestQueue(mContext);
+        mQueue.add(stringRequest);
+    }
+
+    private void filterTable(String text) {
+        if(existingKotBOArrayList !=null && existingKotBOArrayList.size() > 0) {
+            ArrayList<ExistingKotBO> filteredList1 = new ArrayList<>();
+            for (ExistingKotBO item : existingKotBOArrayList) {
+                if (item.getMENUNAME().toLowerCase().startsWith(text)
+                        || item.getRATE().toLowerCase().startsWith(text)
+                        || item.getQTY().toLowerCase().startsWith(text)) {
+                    filteredList1.add(item);
+                }
+            }
+            //Log.e(TAG, "filter: size" + filteredList1.size());
+            // Log.e(TAG, "filter: List" + filteredList1.toString());
+            cancelOrderDetailsAdapter.updateData(mContext, filteredList1);
+        }
+    }
+}
---
+++ app/src/main/java/com/soulsoft/globalrestobar/adapter/CancelOrderDetailsAdapter.java
@@ -0,0 +1,100 @@
+package com.soulsoft.globalrestobar.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.soulsoft.globalrestobar.R;
+import com.soulsoft.globalrestobar.model.existingkot.ExistingKotBO;
+
+import java.util.ArrayList;
+
+public class CancelOrderDetailsAdapter extends RecyclerView.Adapter<CancelOrderDetailsAdapter.TakeOrderViewHolder> {
+
+    Context context;
+    ArrayList<ExistingKotBO> existingKotBOArrayList;
+    private TakeOrderListener takeOrderListener;
+    private final int[] backgroundColors = {R.color.yellow_50,R.color.yellow_100};
+
+    /*public TakeOrderAdapter(Context baseContext, ArrayList<ExistingKotBO> takeOrderArrayList) {
+        this.context = baseContext;
+        this.ExistingKotBOArrayList = takeOrderArrayList;
+    }*/
+
+    public CancelOrderDetailsAdapter(Context context, ArrayList<ExistingKotBO> ExistingKotBOArrayList) {
+        this.context = context;
+        this.existingKotBOArrayList = ExistingKotBOArrayList;
+    }
+
+    @NonNull
+    @Override
+    public TakeOrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.cancel_running_order_details, parent, false);
+        return new TakeOrderViewHolder(view,takeOrderListener);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull TakeOrderViewHolder holder, final int position) {
+
+        int bgColor = ContextCompat.getColor(context, backgroundColors[position % 2]);
+        holder.cardView.setCardBackgroundColor(bgColor);
+
+        int bgColor1 = ContextCompat.getColor(context, backgroundColors[position % 2]);
+        holder.cardView2.setCardBackgroundColor(bgColor1);
+
+        holder.tvCode.setText(existingKotBOArrayList.get(position).getIID());
+        holder.tvMenu.setText(existingKotBOArrayList.get(position).getMENUNAME());
+        holder.tvQuantity.setText("QTY: "+existingKotBOArrayList.get(position).getQTY());
+        //holder.tvServesIn.setText(ExistingKotBOArrayList.get(position).getUnitname());
+        holder.tvAmount.setText("AMT: "+existingKotBOArrayList.get(position).getAMOUNT());
+    }
+
+    @Override
+    public int getItemCount() {
+        return existingKotBOArrayList.size();
+    }
+
+    public class TakeOrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private CardView cardView,cardView2;
+        private TextView tvCode, tvMenu, tvQuantity, tvServesIn, tvAmount;
+        private TakeOrderListener takeOrderListener;
+
+        public TakeOrderViewHolder(@NonNull View itemView, TakeOrderListener takeOrderListener) {
+            super(itemView);
+            cardView=itemView.findViewById(R.id.cardView);
+            cardView2=itemView.findViewById(R.id.cardview2);
+            tvCode = itemView.findViewById(R.id.tv_code);
+            tvMenu = itemView.findViewById(R.id.tv_menu);
+            tvQuantity = itemView.findViewById(R.id.tv_quantity);
+           // tvServesIn = itemView.findViewById(R.id.tv_servesin);
+            tvAmount = itemView.findViewById(R.id.tv_amount);
+            this.takeOrderListener=takeOrderListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            takeOrderListener.onTakeOrderClick(getAdapterPosition());
+        }
+    }
+
+    public interface TakeOrderListener{
+        void onTakeOrderClick(int position);
+    }
+
+    public void updateData(Context context,ArrayList<ExistingKotBO> data) {
+        this.context=context;
+        this.existingKotBOArrayList = data;
+        notifyDataSetChanged();
+    }
+}
